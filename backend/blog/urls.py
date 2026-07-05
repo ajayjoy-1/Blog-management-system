4add2e7df7ce202f78d4f2e2f96b7fc07773a6fb
ajayjoypala@gmail.com <ajayjoypala@gmail.com>
2026-07-05 20:52:03 +0530

    views and url routing

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,3 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PostViewSet, CommentViewSet
 
-urlpatterns = []
+router = DefaultRouter()
+router.register('posts', PostViewSet, basename='post')
+router.register('comments', CommentViewSet, basename='comment')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,6 +1,38 @@
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializers import MyTokenObtainPairSerializer
+from rest_framework import viewsets, permissions
+from .models import Post, Comment
+from .serializers import PostListSerializer, PostDetailSerializer, CommentSerializer
+from .permissions import IsOwnerOrReadOnly
 
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return PostDetailSerializer
+        return PostListSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        queryset = Comment.objects.all()
+        post_id = self.request.query_params.get('post')
+        if post_id is not None:
+            queryset = queryset.filter(post_id=post_id)
+        return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)    
