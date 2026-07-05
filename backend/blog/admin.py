572d3c4fdb82fb6842c0e4af83ef125a6de299f6
ajayjoypala@gmail.com <ajayjoypala@gmail.com>
2026-07-05 16:46:52 +0530

    register the models

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Post, Comment
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at', 'updated_at')
+    list_filter = ('author', 'created_at')
+    search_fields = ('title', 'content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'content', 'created_at')
+    list_filter = ('author', 'post')
+    search_fields = ('content',)
